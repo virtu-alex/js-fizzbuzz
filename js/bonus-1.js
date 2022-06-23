49ffcdc63b2dc6127630a611bf9f0683ee5331c3
virtu-alex <alessio.cordari@gmail.com>
2022-06-23 18:14:33 +0200

    bonus-1

--- js/bonus-1.js
+++ js/bonus-1.js
@@ -1,22 +1,26 @@
 /*BONUS 1:
 Crea un elemento contenitore nel DOM e aggiungendo (attraverso il metodo append(), appendChild() o innerHTML) i vari elementi html con il numero o la stringa corretta da mostrare*/
-
 const bonus = document.getElementById('bonus');
-bonus.innerHTML = '<div>1</div>'
+// bonus.innerHTML = '<div>1</div>'
 
 for (let i = 0; i < 100; i++) {
+    // crea un elemento div nel DOM
+    let div = document.createElement("div");
+    // attacca il div appena creato all'elemento bonus
+    bonus.append(div);
     if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0) {
-        console.log('FizzBuzz')
-        bonus.appendChild('<div>FizzBuzz</div>')
+        console.log('FizzBuzz');
+        // inserisce del testo nell'elemento div appena creato
+        div.append('FizzBuzz');
     } else if ((i + 1) % 3 == 0) {
         console.log('fizz');
-        bonus.appendChild('<div>Fizz</div>')
+        div.append('Fizz');
     } else if ((i + 1) % 5 == 0) {
-        console.log('buzz')
-        bonus.appendChild('<div>Buzz</div>')
+        console.log('buzz');
+        div.append('Buzz');
     } else {
         console.log(i + 1);
-        bonus.appendChild('<div>' + (i + 1) + '</div>')
+        div.append((i + 1));
     }
 }
 
